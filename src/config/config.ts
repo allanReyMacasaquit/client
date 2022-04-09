955e573be1808fc9f66b5611c3a735aa172279f7
allan <allanrey.macasaquit@gmail.com>
2022-04-09 15:10:34 -0400

    Initial Setup

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+export const config = {
+    firebase: {
+        apiKey: "AIzaSyADW5_-rVkSpyY5HsI2HYhwrhVdt3ldTpc",
+        authDomain: "netflix-clone-bf6e4.firebaseapp.com",
+        projectId: "netflix-clone-bf6e4",
+        storageBucket: "netflix-clone-bf6e4.appspot.com",
+        messagingSenderId: "174269576034",
+        appId: "1:174269576034:web:b989e58a0adf365455323b"},
+    server: {
+        url: 'http://localhost:5000'
+    }
+}
